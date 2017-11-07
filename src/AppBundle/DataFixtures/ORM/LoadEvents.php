0c5724424fec65b214ab3074118a3dfe3d862324
Benoît Frisch <benoit@fresh.lu>
2017-11-07 16:04:54 +0100

    Rename DataFixtures load file. The old one was copied from PremiereLu repository.

--- src/AppBundle/DataFixtures/ORM/LoadEvents.php
+++ src/AppBundle/DataFixtures/ORM/LoadEvents.php
@@ -1,22 +1,4 @@
 <?php
-/**
- * This file is part of PremiereLu.
- *
- * Copyright (c) 2017 Benoît FRISCH
- *
- * PremiereLu is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * PremiereLu is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with PremiereLu If not, see <http://www.gnu.org/licenses/>.
- */
 
 namespace AppBundle\DataFixtures\ORM;
 
@@ -28,7 +10,7 @@ use DateTime;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class LoadExamData implements FixtureInterface
+class LoadEvents implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
